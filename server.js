c38cc760391e6c0b51a1e484dd7c7690cae2d7e0
Rishabh Dhiman <33525623+RishabhDhiman@users.noreply.github.com>
2020-06-15 19:56:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var WebSocketServer = require('ws').Server;
  
 //creating a websocket server at port 9090 
-var wss = new WebSocketServer({port: 9090}); 
+var wss = new WebSocketServer({port: (process.env.PORT || 5000}); 
 
 //all connected to the server users 
 var users = {};
