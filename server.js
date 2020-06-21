0ef783ea2ec5fc68f5565aea7382816c2f9903b1
rishabhdhiman <rishabhdhiman12345@gmail.com>
2020-06-21 15:38:13 +0530

    Signaling sever working perfeect.

--- server.js
+++ server.js
@@ -1,180 +1,183 @@
-'use strict';
-//require our websocket library 
-var WebSocketServer = require('ws').Server;
+"use strict";
+var WebSocketServer = require("ws").Server;
+var ArrayList = require("arraylist");
 var wrtc = require("wrtc");
-//creating a websocket server at port 9090 
-var wss = new WebSocketServer({port: 9090});
-
+var wss = new WebSocketServer({ port: 9090 });
 var RTCPeerConnection = wrtc.RTCPeerConnection;
 var RTCSessionDescription = wrtc.RTCSessionDescription;
-//all connected to the server users 
 var users = {};
 
-var pc ;
+//var list = new ArrayList();
 
-//when a user connects to our sever 
-wss.on('connection', function(connection) {
+var pc;
 
-   console.log("User connected");
+//when a user connects to our sever
+wss.on("connection", function (connection) {
+  console.log("User connected");
 
-   //when server gets a message from a connected user
-   connection.on('message', function(message) { 
+  //when server gets a message from a connected user
+  connection.on("message", function (message) {
+    var data;
+    //accepting only JSON messages
+    try {
+      data = JSON.parse(message);
+    } catch (e) {
+      console.log("Invalid JSON " + message);
+      data = {};
+    }
 
-      var data; 
-      //accepting only JSON messages 
-      try {
-         data = JSON.parse(message); 
-      } catch (e) { 
-         console.log("Invalid JSON "+message); 
-         data = {}; 
-      } 
+    switch (data.type) {
+      case "login":
+        login(connection, data);
+        break;
 
-      //switching type of the user message 
-      switch (data.type) { 
-         //when a user tries to login 
+      case "offer":
+        offer(data);
+        break;
 
-         case "login": 
-         console.log("User logged", data.name); 
+      case "answer":
+        answer(data);
+        break;
 
-            //if anyone is logged in with this username then refuse 
-            if(users[data.name]) {
-               sendTo(connection, {
-                  type: "login", 
-                  success: false 
-               }); 
-            } else { 
-               //save user connection on the server 
-               users[data.name] = connection; 
-               connection.name = data.name; 
+      case "candidate":
+        candidate(data);
+        break;
+      case "leave":
+        leave(connection);
+        break;
+      default:
+        defaultCase(connection);
+        break;
+    }
+  });
 
-               sendTo(connection, { 
-                  type: "login", 
-                  success: true 
-               }); 
-            } 
+  connection.on("close", function () {
+    if (users[connection.roomName] && users[connection.roomName].userCount == 2) {
+      users[connection.roomName].userCount = 0;
+      if (connection.isInitiator) {
+        sendTo(users[connection.roomName].users[1].connection, {
+          type: "leave",
+        });
+        users[connection.roomName].users[1].connection.close();
+        delete users[connection.roomName];
+      } else {
+        sendTo(users[connection.roomName].users[0].connection, {
+          type: "leave",
+        });
+        users[connection.roomName].users[0].connection.close();
+        delete users[connection.roomName];
+      }
+    }
+  });
 
-            break; 
+  connection.send('{"type":"connection"}');
+});
 
-            case "offer": 
-            //for ex. UserA wants to call UserB 
-            console.log("Sending offer to: ", data.name); 
+function login(connection, data) {
+  console.log("User logged", data.name + " in room " + data.roomName);
+  if (!users[data.roomName]) {
+    connection.isInitiator = true;
+    connection.roomName = data.roomName;
+    users[data.roomName] = {};
+    users[data.roomName].userCount = 0;
+    users[data.roomName].users = [];
+    users[data.roomName].users[users[data.roomName].userCount] = {};
+    users[data.roomName].users[users[data.roomName].userCount].name = data.name;
+    users[data.roomName].users[
+      users[data.roomName].userCount
+    ].connection = connection;
+    users[data.roomName].userCount++;
+    sendTo(connection, {
+      type: "initiator",
+      initiator: true,
+    });
+  } else {
+    if (users[data.roomName].userCount == 2) {
+      sendTo(connection, {
+        type: "roomFull",
+        answer: "Room is full",
+      });
+    } else {
+      connection.isInitiator = false;
+      connection.roomName = data.roomName;
+      users[data.roomName].users[users[data.roomName].userCount] = {};
+      users[data.roomName].users[users[data.roomName].userCount].name =
+        data.name;
+      users[data.roomName].users[
+        users[data.roomName].userCount
+      ].connection = connection;
+      console.log("Object", users);
+      users[data.roomName].userCount++;
+      sendTo(connection, {
+        type: "initiator",
+        initiator: false,
+      });
 
-            //if UserB exists then send him offer details 
-            var conn = users[data.name];
+      sendTo(users[data.roomName].users[0].connection, {
+        type: "peerArrived",
+      });
+    }
+  }
+}
 
-            if(conn != null) { 
-               //setting that UserA connected with UserB 
-               connection.otherName = data.name; 
+function offer(data) {
+  users[data.roomName].users[0].sessionDescription = data.sessionDescription;
+  sendTo(users[data.roomName].users[1].connection, {
+    type: "offer",
+    sessionDescription: data.sessionDescription,
+  });
+}
 
-               sendTo(conn, { 
-                  type: "offer", 
-                  offer: data.offer, 
-                  name: connection.name 
-               }); 
-            } 
+function answer(data) {
+  users[data.roomName].users[1].sessionDescription = data.sessionDescription;
+  sendTo(users[data.roomName].users[0].connection, {
+    type: "answer",
+    sessionDescription: data.sessionDescription,
+  });
+}
 
-            break;  
+function candidate(data) {
+  if (data.isInitiator) {
+    users[data.roomName].users[0].iceCandidate = data.iceCandidate;
+    sendTo(users[data.roomName].users[1].connection, {
+      type: "candidate",
+      iceCandidate: data.iceCandidate,
+    });
+  } else {
+    users[data.roomName].users[1].iceCandidate = data.iceCandidate;
+    sendTo(users[data.roomName].users[0].connection, {
+      type: "candidate",
+      iceCandidate: data.iceCandidate,
+    });
+  }
+}
 
-            case "answer": 
-            console.log("Sending answer to: ", data.name); 
-            //for ex. UserB answers UserA 
-            var conn = users[data.name]; 
+function defaultCase(connection) {
+  sendTo(connection, {
+    type: "error",
+    message: message,
+  });
+}
+function leave(connection) {
+  console.log("Disconnecting from", data.name);
+  var conn = users[data.name];
+  conn.otherName = null;
 
-            if(conn != null) { 
-               connection.otherName = data.name; 
-               sendTo(conn, { 
-                  type: "answer", 
-                  answer: data.answer 
-               }); 
-            } 
+  //notify the other user so he can disconnect his peer connection
+  if (conn != null) {
+    sendTo(conn, {
+      type: "leave",
+    });
+  }
+}
 
-            break;  
-
-            case "candidate":
-            pc = new RTCPeerConnection({
-               iceServers: [
-               {
-                urls: ['stun:stun.l.google.com:19302']
-             }
-             ]
-          });
-            pc.onicecandidate = function(candidate) {
-               if (candidate.candidate) {
-                 console.log(candidate.candidate.candidate);
-                 if (candidate.candidate.candidate.indexOf('typ srflx') > -1) {
-                   finish();
-                }
-             }
-          };
-/* 
-             pc.onicegatheringstatechange = function() {
-               if (pc.iceGatheringState === 'complete') {
-                 finish();
-               }
-            };*/
-            
-            pc.createDataChannel('test');
-            pc.createOffer().then(function(e) {
-               pc.setLocalDescription(new RTCSessionDescription(e));
-            });
-            break;  
-
-            case "leave": 
-            console.log("Disconnecting from", data.name); 
-            var conn = users[data.name]; 
-            conn.otherName = null; 
-
-            //notify the other user so he can disconnect his peer connection 
-            if(conn != null) { 
-               sendTo(conn, { 
-                  type: "leave" 
-               }); 
-            }  
-
-            break;  
-
-            default: 
-            sendTo(connection, { 
-               type: "error", 
-               message: message
-            }); 
-
-            break; 
-         }  
-      });  
-
-   //when user exits, for example closes a browser window 
-   //this may help if we are still in "offer","answer" or "candidate" state 
-   connection.on("close", function() { 
-
-      if(connection.name) { 
-         delete users[connection.name]; 
-
-         if(connection.otherName) { 
-            console.log("Disconnecting from ", connection.otherName);
-            var conn = users[connection.otherName]; 
-            conn.otherName = null;  
-
-            if(conn != null) { 
-               sendTo(conn, { 
-                  type: "leave" 
-               });
-            }  
-         } 
-      } 
-   });  
-
-   connection.send("Welcome Rishabh"); 
-
-});  
-
-function finish() {
-   if (pc.signalingState === 'closed') {
-     return;
+function finish(connection) {
+  if (pc.signalingState === "closed") {
+    return;
   }
   pc.close();
 }
 
-function sendTo(connection, message) { 
-   connection.send(JSON.stringify(message)); 
+function sendTo(connection, message) {
+  connection.send(JSON.stringify(message));
 }
